86066b1f8fb063cf9d45562b5d345cb8440a86cc
James Baes <jeb5002@shaw.ca>
2025-05-30 17:43:12 -0600

    Completed Part C and Part D

--- cdestructuring.js
+++ cdestructuring.js
@@ -1,13 +1,16 @@
 const student = {
-	name: "Joe Blow",
-	age: 25,
-	country: "Canada"
+  name: "Joe Blow",
+  age: 25,
+  country: "Canada",
 };
 
-// ...
+const { name, age } = student;
 
 console.log(name);
 console.log(age);
+
+//This is for the way it was mentioned on the dropbox.
+console.log(`Hello! My name is ${name} and I am ${age} years old.`);
 
 /*
  * Expected output:
--- dspreadoperator.js
+++ dspreadoperator.js
@@ -2,7 +2,14 @@ const a = [1, 2, 3];
 const b = [4, 5, 6];
 const c = [7, 8, 9];
 
-// ...
+/*In terms of the dropbox, it states that two arrays are taken as arguments.
+However the output here shows three arrays so I added the third parameter to the function*/
+
+const mergeArrays = (arr1, arr2, arr3) => {
+  return [...arr1, ...arr2, ...arr3];
+};
+
+merged = mergeArrays(a, b, c);
 
 console.log(merged);
 
